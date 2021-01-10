1f4836fbdd254752696051ee666ca9b7f3f00f57
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-01-10 19:20:09 +0300

    add create_bmp_info_header

--- src/bmp.c
+++ src/bmp.c
@@ -6,7 +6,7 @@
 /*   By: akasha <akasha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/10 12:38:58 by user              #+#    #+#             */
-/*   Updated: 2021/01/10 18:41:33 by akasha           ###   ########.fr       */
+/*   Updated: 2021/01/10 19:17:59 by akasha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,25 @@ void    create_bmp_file_header(t_config *config)
 
 void    create_bmp_info_header(t_config *config)
 {
+    int width;
+    int height;
+
+    width = config->params.window_width;
+    height = config->params.window_height;
     reset_to_zero(config->bmp.file_header, 40);
+    config->bmp.info_header[0] = (unsigned char)(40);
+
+    config->bmp.info_header[4] = (unsigned char)(width);
+    config->bmp.info_header[5] = (unsigned char)(width >> 8);
+    config->bmp.info_header[6] = (unsigned char)(width >> 16);
+    config->bmp.info_header[7] = (unsigned char)(width >> 24);
+
+    config->bmp.info_header[8] = (unsigned char)(height);
+    config->bmp.info_header[9] = (unsigned char)(height >> 8);
+    config->bmp.info_header[10] = (unsigned char)(height >> 16);
+    config->bmp.info_header[11] = (unsigned char)(height >> 24);
+
+    config->bmp.info_header[12] = (unsigned char)(1);
     
+    config->bmp.info_header[14] = (unsigned char)(3 * 8);
 }
