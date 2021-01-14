d4b730c423eddfaafa96557d0f01023d945cc75f
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-01-14 20:00:27 +0300

    norm sort_sprite

--- src/sort_sprites.c
+++ src/sort_sprites.c
@@ -6,7 +6,7 @@
 /*   By: akasha <akasha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/07 18:48:04 by akasha            #+#    #+#             */
-/*   Updated: 2021/01/08 19:11:38 by akasha           ###   ########.fr       */
+/*   Updated: 2021/01/14 20:00:06 by akasha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@ void	swap_elems(double *elem_1, double *elem_2)
 	*elem_2 = swap;
 }
 
-int		partition(t_config *config ,int left, int right)
+int		partition(t_config *config, int left, int right)
 {
-	int i;
-	int j;
-	double pivot;
+	int		i;
+	int		j;
+	double	pivot;
 
 	i = left;
 	j = left;
@@ -34,25 +34,29 @@ int		partition(t_config *config ,int left, int right)
 	{
 		if (config->sprite.sprite_distanse[j] > pivot)
 		{
-			swap_elems(&config->sprite.sprite_distanse[i], &config->sprite.sprite_distanse[j]);
-			swap_elems((&config->sprite.sprite_order[i]), &config->sprite.sprite_order[j]);
+			swap_elems(&config->sprite.sprite_distanse[i],
+						&config->sprite.sprite_distanse[j]);
+			swap_elems((&config->sprite.sprite_order[i]),
+						&config->sprite.sprite_order[j]);
 			i++;
 		}
 		j++;
 	}
-	swap_elems(&config->sprite.sprite_distanse[i], &config->sprite.sprite_distanse[right]);
-	swap_elems((&config->sprite.sprite_order[i]), &config->sprite.sprite_order[right]);
+	swap_elems(&config->sprite.sprite_distanse[i],
+			&config->sprite.sprite_distanse[right]);
+	swap_elems((&config->sprite.sprite_order[i]),
+				&config->sprite.sprite_order[right]);
 	return (i);
 }
 
-void 	sort_sprites(t_config *config, int left, int right)
-{  
+void	sort_sprites(t_config *config, int left, int right)
+{
 	int pivot;
 
-    if (left < right)
-    {
+	if (left < right)
+	{
 		pivot = partition(config, left, right);
-        sort_sprites(config, left, pivot - 1);
-        sort_sprites(config, pivot + 1, right);
-    }
+		sort_sprites(config, left, pivot - 1);
+		sort_sprites(config, pivot + 1, right);
+	}
 }
